d42e4499867dab8b36bc5c9eb5cc673e7fa45a09
blacwolf4 <blacwolf4@gmail.com>
2023-10-19 02:17:51 +0000

    added cleanup bash script

---
+++ cleanup-all.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+
+# delete routers
+# sudo ip netns del 'variable'
+sudo ip netns del howard-nrtr
+sudo ip netns del michael-srtr
+sudo ip netns del camilo-ertr
+sudo ip netns del ming-wrtr
+sudo ip netns del lnd-nat-cor
+
+# delete hosts
+# sudo ip netns del 'variable'
+sudo ip netns del chrys-nhost
+sudo ip netns del david-shost
+sudo ip netns del abdul-ehost
+sudo ip netns del mark-whost
+
+
+# delete bridges
+# sudo ip link del dev 'variable'
+sudo ip link del dev North-bridge
+sudo ip link del dev South-bridge
+sudo ip link del dev East-bridge
+sudo ip link del dev West-bridge
+
+
+# delete interfaces
+# sudo ip netns exec bowser ip link delete bowser2net
+
+
+
+# delete veths
