d12ae6b910bbcb7022d869f41d0337a53a7c2e00
diegozpineda <dzpineda86@gmail.com>
2023-10-19 05:30:52 +0000

    Created net_deployment.yml playbook, which imports tasks lists. Broke out tasks into seperate tasks list files (see net_deployment.yml). Also created task list for creating lan interfaces and validated host ip address assignment works. Created an updated data structure for testing which includes a veth structure for ip address assignment (net_datastructure-new2.yml). This can be merged to primary net_datastructure.yml when we are confident the structure works as intended.

--- del-project.sh
+++ del-project.sh
@@ -2,13 +2,25 @@
 #
 # del proj stuff
 #
+echo -e "Remove all netns"
 a=$(sudo ip netns list)
 for i in $a; do sudo ip netns del $i; done
-brctl
-a=$(brctl show | grep "bridge" | tail -4 | cut -f 1)
+echo -e "\nValidate all netns have been removed\n\n"
+sudo ip netns list
+
+echo -e "Remove all bridge interfaces\n" 
+a=$(brctl show | grep "\-bridge" | cut -f 1)
 for i in $a; do sudo ip link del dev $i; done
+echo -e "Validate all bridges have been removed\n\n"
+brctl show
+
+echo -e "Remove all remaining veth interfaces\n"
+a=$(ip link show type veth | cut -d ' ' -f 2 | cut -f 1 -d '@' | egrep -v veth)
+for i in $a; do sudo ip link del $i; done
+echo -e "Validate all veth interfaces have been removed\n\n"
+sudo ip link show type veth
 
 # validate
-sudo ip netns list
-brctl show
+#sudo ip netns list
+#brctl show
 # EOF
