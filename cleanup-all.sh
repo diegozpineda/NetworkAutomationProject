bed9659096e666abe129850a60dc1b7e480f1b75
blacwolf4 <blacwolf4@gmail.com>
2023-10-19 02:47:35 +0000

    added cleanup bash scripts for individual types

--- cleanup-all.sh
+++ cleanup-all.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 
+# show routers
+# ip netns
 # delete routers
 # sudo ip netns del 'variable'
 sudo ip netns del howard-nrtr
@@ -9,6 +11,8 @@ sudo ip netns del camilo-ertr
 sudo ip netns del ming-wrtr
 sudo ip netns del lnd-nat-cor
 
+# show hosts
+# ip netns
 # delete hosts
 # sudo ip netns del 'variable'
 sudo ip netns del chrys-nhost
@@ -17,6 +21,8 @@ sudo ip netns del abdul-ehost
 sudo ip netns del mark-whost
 
 
+# show bridges
+# ip link show type bridge
 # delete bridges
 # sudo ip link del dev 'variable'
 sudo ip link del dev North-bridge
@@ -25,9 +31,10 @@ sudo ip link del dev East-bridge
 sudo ip link del dev West-bridge
 
 
-# delete interfaces
-# sudo ip netns exec bowser ip link delete bowser2net
-
-
-
+# show veths
+# ip link show type veth
 # delete veths
+# sudo ip netns exec bowser ip link delete bowser2net
+# deleting the bridges in the step above will automatically delete veths
+
+
---
+++ cleanup-rtr.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+# show routers
+# ip netns
+# delete routers
+# sudo ip netns del 'variable'
+sudo ip netns del howard-nrtr
+sudo ip netns del michael-srtr
+sudo ip netns del camilo-ertr
+sudo ip netns del ming-wrtr
+sudo ip netns del lnd-nat-cor
