1d8022defe08995e1e41e4ccfdc69768cdb839f6
blacwolf4 <blacwolf4@gmail.com>
2023-10-19 18:47:09 +0000

    new commit for ryan_branch

--- gitcap.sh
+++ gitcap.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-read -p "Comments for upload to  project?: " name
+#read -p "Comments for upload to  project?: " name
 
-git checkout -b ryan_branch
-git remote add upstream https://github.com/diegozpineda/NetworkAutomationProject
+#git checkout -b ryan_branch
+#git remote add upstream https://github.com/diegozpineda/NetworkAutomationProject
 
 git status
 git add *
+git status
+
+read -p "Comments for upload to  project?: " name
 git commit -m "$name"
 git push
 
