98d2bf4314ec6f2feb22feb369de277ea913128e
diegozpineda <dzpineda86@gmail.com>
2023-10-23 03:37:47 +0000

    Moved non critical backups into backups/ directory. Validated cap_deployment.py deploys network with changes.

---
+++ backup/cleanup-hosts.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+# show hosts
+# ip netns
+# delete hosts
+# sudo ip netns del 'variable'
+sudo ip netns del chrys-nhost
+sudo ip netns del david-shost
+sudo ip netns del abdul-ehost
+sudo ip netns del mark-whost
